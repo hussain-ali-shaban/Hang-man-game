a950d387151741306f410718ea6baf037bb7d372
hussain-ali-shaban <hussain.ali.shaban@gmail.com>
2025-05-13 11:37:25 +0300

    start javascriptpart

---
+++ app.js
@@ -0,0 +1,9 @@
+const letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"];
+let letterInside = document.querySelector(".letters");
+
+letters.forEach(letter =>{
+   let theSpan = document.createElement("span");
+theSpan.textContent = letters; 
+
+
+})
