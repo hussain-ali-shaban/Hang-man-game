a49e0a283ae76b56d5c99b89c132d302b0d0bdd2
hussain-ali-shaban <hussain.ali.shaban@gmail.com>
2025-05-22 12:56:31 +0300

    display first image

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
+const keyboard = querySelector(".kbd");
+
+for(i=0;i<=26;i++){
+    
+
+}
+
+
+
+
 const letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"];
 let letterInside = document.querySelector(".letters");
 
 letters.forEach(letter =>{
-   let theSpan = document.createElement("span");
-theSpan.textContent = letter; 
+    let theSpan = document.createElement("span");
+ theSpan.textContent = letter; 
 
 
 
-  letterInside.appendChild(theSpan);
+  //  letterInside.appendChild(theSpan);
 
-})
+ })
 const theWords =  ["apple","cherry","banana","berry"];
+
+document.addEventListener('keypress',(event) => {
+  console.log(event.key.toLowerCase());
+});
+  console.log(theWords)
+
+// const lettersInWord = word.length;
+
+ theWords.forEach(word => {
+if( word==='apple'|| word==='berry'){
+    console.log('- - - - -')
+    console.log(word)
+ }
+  if (  word=== 'cherry'|| word==='banana'){
+   console.log('- - - - - -')
+  console.log(word)
+  } }) 
+
+ 
+  
+
+
+ 
+ 
+
+ 
+
+
+    
+
+    
+
   
 
